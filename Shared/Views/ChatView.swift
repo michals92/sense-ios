371db135a64ec9654f36c45ac4b9dbe9bdbf0c93
Michal Simik <michal.simik@gmail.com>
2022-03-05 12:34:12 +0100

    feat: hide tabbar in chat thread

--- Shared/Views/ChatView.swift
+++ Shared/Views/ChatView.swift
@@ -12,29 +12,36 @@ struct ChatView: View {
     @ObservedObject var viewModel: MainViewModel
     @State var message: String = ""
 
-    var body: some View {
-            if let splTokens = viewModel.splTokens {
-                VStack {
-                    List {
-                        ForEach(homeData.messages) {
-                            Text($0.message)
-                        }
-                    }
-                    HStack {
-                        TextField("Message", text: $message)
-                        Button {
-                            homeData.sendMessage(message: message)
-                        } label: {
-                            Text("send")
-                        }
-                    }
-                    .padding()
-                }.onAppear {
-                    homeData.onAppear()
-                }
+    @State var tabBarController: UITabBarController?
 
-                .navigationTitle("Chat with Sarka")
-                .navigationBarTitleDisplayMode(.inline)
+    var body: some View {
+        VStack {
+            List {
+                ForEach(homeData.messages) {
+                    Text($0.message)
+                }
+            }
+            HStack {
+                TextField("Message", text: $message)
+                Button {
+                    homeData.sendMessage(message: message)
+                } label: {
+                    Text("send")
+                }
+            }
+            .padding()
+        }.onAppear {
+            homeData.onAppear()
+        }
+
+        .navigationTitle("Chat with Sarka")
+        .navigationBarTitleDisplayMode(.inline)
+        .introspectTabBarController { tabBarController in
+            tabBarController.tabBar.isHidden = true
+            self.tabBarController = tabBarController
+        }
+        .onDisappear {
+            tabBarController?.tabBar.isHidden = false
         }
     }
 }
